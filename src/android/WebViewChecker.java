c4f51e7f7f6c55c23ef07eb335789073e75463f8
illia.korobov <illia.korobov@tu-ilmenau.de>
2022-07-11 12:19:10 +0200

    revert init functions

--- src/android/WebViewChecker.java
+++ src/android/WebViewChecker.java
@@ -46,8 +46,30 @@ public class WebViewChecker extends CordovaPlugin {
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    if (action.equals("isAppEnabled")) {
+      String packageName = args.getString(0);
+      this.isAppEnabled(packageName, callbackContext);
+
+      return true;
+    }
+
+    if (action.equals("getAppPackageInfo")) {
+      String packageName = args.getString(0);
+      this.getAppPackageInfo(packageName, callbackContext);
+
+      return true;
+    }
+
+    if (action.equals("openGooglePlayPage")) {
+      String packageName = args.getString(0);
+      this.openGooglePlayPage(packageName, callbackContext);
+
+      return true;
+    }
+
     if (action.equals("getCurrentWebViewPackageInfo")) {
       this.getCurrentWebViewPackageInfo(callbackContext);
+
       return true;
     }
 
@@ -107,6 +129,52 @@ public class WebViewChecker extends CordovaPlugin {
       System.out.println("getCurrentWebViewPackageInfo pInfo set");
     }
     return pInfo;
+  }
+
+  /**
+   * Returns partial package information about the specified package.
+   */
+  public void getAppPackageInfo(String packagename, CallbackContext callbackContext) throws org.json.JSONException {
+    PackageInfo pInfo = null;
+    JSONObject responseObject = new JSONObject();
+    PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+
+    try {
+      pInfo = packageManager.getPackageInfo(packagename, 0);
+    } catch (PackageManager.NameNotFoundException e) {
+      callbackContext.error("Package not found");
+    }
+
+    responseObject.put("packageName", pInfo.packageName);
+    responseObject.put("versionName", pInfo.versionName);
+    responseObject.put("versionCode", pInfo.versionCode);
+
+    callbackContext.success(responseObject);
+  }
+
+  public void isAppEnabled(String packagename, CallbackContext callbackContext) {
+    PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+
+    try {
+      Boolean enabled = packageManager.getApplicationInfo(packagename, 0).enabled;
+      callbackContext.success(enabled.toString());
+    } catch (PackageManager.NameNotFoundException e) {
+      callbackContext.error("Package not found");
+    }
+  }
+
+  private void openGooglePlayPage(String packagename, CallbackContext callbackContext) throws android.content.ActivityNotFoundException {
+    Context context = this.cordova.getActivity().getApplicationContext();
+    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packagename));
+
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+    try {
+      context.startActivity(intent);
+      callbackContext.success();
+    } catch (Exception e) {
+      callbackContext.error("Cannot open Google Play. (" + e.getMessage() + ")");
+    }
   }
 
   private void openWebViewWarning() {
